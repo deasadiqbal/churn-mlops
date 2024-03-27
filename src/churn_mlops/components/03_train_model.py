2e7df447df77f9b7a95db88738524e5b0ca87cf9
deasadiqbal <asad95298@gmail.com>
2024-03-27 21:12:35 +0500

    training and evaluation done

--- src/churn_mlops/components/03_train_model.py
+++ src/churn_mlops/components/03_train_model.py
@@ -1,4 +1,5 @@
 import joblib
+import mlflow
 import pandas as pd
 from urllib.parse import urlparse
 
@@ -7,7 +8,21 @@ from churn_mlops.constants import *
 from churn_mlops.utils.common import read_yaml
 
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, f1_score,recall_score, precision_score, confusion_matrix, classification_report
 
+
+# function to evaluate the model
+def evaluation_metrics(y_test, pred, avg_method):
+    accuracy = accuracy_score(y_test, pred)
+    precision = precision_score(y_test, pred, average=avg_method)
+    recall = recall_score(y_test, pred, average=avg_method) 
+    f1 = f1_score(y_test, pred, average=avg_method)
+    cm = confusion_matrix(y_test, pred) 
+
+    target_names = ['0', '1']
+    classification_rep = classification_report(y_test, pred, target_names=target_names)
+
+    return accuracy, precision, recall, f1, cm, classification_rep
 
 # function to load features and target
 def load_features_target(df, target):
@@ -15,11 +30,11 @@ def load_features_target(df, target):
     y = df[[target]]
     return x,y
 
-# function to traine and evaluate the model
+
 def train_and_evaluate(config_path):
-    # read the config 
+    # read the config and params
     config = read_yaml(config_path)
-    
+
     #data paths and target variable
     train_data_path = config.transformed_data_config.churn_train_data_path
     test_data_path = config.transformed_data_config.churn_test_data_path
@@ -31,15 +46,37 @@ def train_and_evaluate(config_path):
 
     # read the train and test data
     train_df = pd.read_csv(train_data_path)
-    test_df = pd.read_csv(test_data_path)
     x_train, y_train = load_features_target(train_df, target)
+
+    # read the test data
+    test_df = pd.read_csv(test_data_path)
     x_test, y_test = load_features_target(test_df, target)
 
-    model = RandomForestClassifier(max_depth=max_depth, n_estimators=n_estimators)
-    model.fit(x_train, y_train)
+    # random_forest_model = joblib.load(config.model_dir)
+    random_forest_model = RandomForestClassifier(max_depth=max_depth, n_estimators=n_estimators)
+    random_forest_model.fit(x_train, y_train)
+    
 
-    joblib.dump(model, config.model_dir)
-    logger.info(f"Model is saved at {config.model_dir}")
+    mlflow.set_tracking_uri(config.mlflow_config.mlflow_uri)
+    tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+    with mlflow.start_run(run_name=config.mlflow_config.experiment_name):
+        pred = random_forest_model.predict(x_test)
+
+        accuracy, precision, recall, f1, cm, classification_rep = evaluation_metrics(y_test, pred, "weighted")
+    
+        mlflow.log_param("max_depth",max_depth)
+        mlflow.log_param("n_estimators", n_estimators)
+
+        mlflow.log_metric("accuracy", accuracy)
+        mlflow.log_metric("precision", precision)
+        mlflow.log_metric("recall", recall)
+        mlflow.log_metric("f1_score", f1)
+
+        if tracking_url_type_store != "file":
+                mlflow.sklearn.log_model(random_forest_model, "model", registered_model_name="RandomForestModel")
+        else:
+            mlflow.sklearn.log_model(random_forest_model, "model")
 
 if __name__ == "__main__":
-    train_and_evaluate(CONFIG_FILE_PATH) 
+    train_and_evaluate(CONFIG_FILE_PATH)
